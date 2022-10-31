04c78b449ea460287060df8019fac1d555cc466a
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-10-31 21:12:58 +1300

    13.22

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,3 +1,5 @@
+const { tokenExtractor } = require("../util/middleware");
+
 const jwt = require("jsonwebtoken");
 const router = require("express").Router();
 
@@ -39,20 +41,6 @@ router.get("/", async (req, res) => {
 
   res.json(blogs);
 });
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch {
-      return res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    return res.status(401).json({ error: "token missing" });
-  }
-  next();
-};
 
 router.post("/", tokenExtractor, async (req, res) => {
   try {
--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -1,3 +1,5 @@
+const { tokenExtractor } = require("../util/middleware");
+
 const router = require("express").Router();
 
 const { Blog, User, ReadingList } = require("../models");
@@ -16,6 +18,19 @@ router.post("/", readingListFinder, async (req, res) => {
     res.json(readingListEntry);
   } catch (error) {
     return res.status(400).json({ error });
+  }
+});
+
+router.put("/:id", tokenExtractor, async (req, res) => {
+  const readingList = await ReadingList.findByPk(req.params.id);
+  const user = await User.findByPk(req.decodedToken.id);
+  console.log("ids", user.id, readingList.userId);
+  if (readingList && user && user.id === readingList.userId) {
+    readingList.read = req.body.read;
+    await readingList.save();
+    res.json({ read: req.body.read });
+  } else {
+    res.status(404).end();
   }
 });
 
